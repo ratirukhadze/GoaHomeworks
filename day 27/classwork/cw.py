90b5f368646c0ad0ec23432b33f62bef903b0c2a
ratirukhadze <157207456+ratirukhadze@users.noreply.github.com>
2024-12-27 17:44:52 +0400

    Add files via upload

---
+++ day 27/classwork/cw.py
@@ -0,0 +1,26 @@
+m = 60
+s = 60
+d = 24
+
+print(m * 60)
+print(24 * 60)
+print(1440 * 60)
+
+
+
+print('Nickname: Master')
+print('Score:', 99)
+
+co = 'Portugal'
+ca = 'Lisbon'
+
+print('Counrty:', co)
+print('Capital city:', ca)
+
+
+balance = input('Enter Your Balance')
+withdraw = input('Enter The Withdraw Amount')
+
+
+
+print(balance, - withdraw)
